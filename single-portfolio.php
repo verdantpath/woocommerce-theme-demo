b80bcd51e46ab1c52302a86c3b218150e96ef3a1
Matt <m_dittmann@yahoo.com>
2017-12-14 21:43:13 -0800

    update single-portfolio.php

--- single-portfolio.php
+++ single-portfolio.php
@@ -7,28 +7,28 @@
       <!-- Primary Column -->
       <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
         <div class="primary">
-    		
-    		<?php the_field('images'); ?>		      		      
-    
+
+    		<?php the_field('images'); ?>
+
 		</div>
 	  </div>
-	
+
 	  <!-- Secondary Column -->
 	  <div class="small-12 medium-4 medium-pull-8 columns">
 		<div class="secondary">
-			
+
 			<h1><?php the_title(); ?></h1>
-			<p><?php the_field('description'); ?></p>
-		
+			<p><?php the_field('description'); ?></p> 
+
 			<hr>
 
 			<p>
 				<?php previous_post_link(); ?> -
-				<a href="<?php bloginfo('url'); ?>/portfolio">Back to Portfolio</a> - 
+				<a href="<?php bloginfo('url'); ?>/portfolio">Back to Portfolio</a> -
 				<?php next_post_link(); ?>
 			</p>
 
-		</div>	
+		</div>
 	  </div>
 	<?php endwhile; endif; ?>
   </div>
