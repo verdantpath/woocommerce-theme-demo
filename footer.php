79275b9ffbf49997d8dc629f198f171260c4cbb7
Matt <m_dittmann@yahoo.com>
2017-12-15 21:17:23 -0800

    update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
     <div class="footer-clear"></div>
-<footer class="row no-max pad">           
-  <p>Copyright <?php echo date('Y'); ?></p>
+<footer class="row no-max pad">
+
+  <div class="small-12 medium-6 columns">
+    <p>Copyright <?php echo date('Y'); ?></p>
+  </div>
+  <div class="small-12 medium-6 columns right">
+    <?php do_action( 'wpt_footer' ); ?>
+  </div>
 </footer>
 
   <?php wp_footer(); ?>
