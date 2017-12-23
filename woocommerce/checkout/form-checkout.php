f16fd399ee4304aa81618f25b74daef44ed5ecc2
Matt <m_dittmann@yahoo.com>
2017-12-22 17:18:32 -0800

    update form-checkout.php

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -57,7 +57,10 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
 	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+		<?php
+		
+		 do_action( 'woocommerce_checkout_order_review' );
+		 ?>
 	</div>
 
 	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
